74d40018225d54f0e62935bc8cb7faddbeba608d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-04 12:25:32 +0800

    move win32apis to single class.

--- SmileWei.EmbeddedApp/AppContainer.cs
+++ SmileWei.EmbeddedApp/AppContainer.cs
@@ -144,7 +144,7 @@ namespace SmileWei.EmbeddedApp
         {
             if (m_AppProcess != null)
             {
-                MoveWindow(m_AppProcess.MainWindowHandle, 0, 0, this.Width, this.Height, true);
+                Win32API.MoveWindow(m_AppProcess.MainWindowHandle, 0, 0, this.Width, this.Height, true);
             }
             base.OnResize(eventargs);
         }
@@ -210,137 +210,7 @@ namespace SmileWei.EmbeddedApp
         
         #endregion 属性
 
-        #region Win32 API
-        [DllImport("user32.dll", EntryPoint = "GetWindowThreadProcessId", SetLastError = true,
-             CharSet = CharSet.Unicode, ExactSpelling = true,
-             CallingConvention = CallingConvention.StdCall)]
-        private static extern long GetWindowThreadProcessId(long hWnd, long lpdwProcessId);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-        [DllImport("user32.dll", EntryPoint = "GetWindowLongA", SetLastError = true)]
-        private static extern long GetWindowLong(IntPtr hwnd, int nIndex);
-
-        public static IntPtr SetWindowLong(HandleRef hWnd, int nIndex, int dwNewLong)
-        {
-            if (IntPtr.Size == 4)
-            {
-                return SetWindowLongPtr32(hWnd, nIndex, dwNewLong);
-            }
-            return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
-        }
-        [DllImport("user32.dll", EntryPoint = "SetWindowLong", CharSet = CharSet.Auto)]
-        public static extern IntPtr SetWindowLongPtr32(HandleRef hWnd, int nIndex, int dwNewLong);
-        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", CharSet = CharSet.Auto)]
-        public static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, int dwNewLong);
- 
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern long SetWindowPos(IntPtr hwnd, long hWndInsertAfter, long x, long y, long cx, long cy, long wFlags);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool MoveWindow(IntPtr hwnd, int x, int y, int cx, int cy, bool repaint);
-
-        [DllImport("user32.dll", EntryPoint = "PostMessageA", SetLastError = true)]
-        private static extern bool PostMessage(IntPtr hwnd, uint Msg, uint wParam, uint lParam);
-
-        /// <summary>
-        /// 获取系统错误信息描述
-        /// </summary>
-        /// <param name="errCode">系统错误码</param>
-        /// <returns></returns>
-        public static string GetLastError()
-        {
-            var errCode = Marshal.GetLastWin32Error();
-            IntPtr tempptr = IntPtr.Zero;
-            string msg = null;
-            FormatMessage(0x1300, ref tempptr, errCode, 0, ref msg, 255, ref tempptr);
-            return msg;
-        }
-        /// <summary>
-        /// 获取系统错误信息描述
-        /// </summary>
-        /// <param name="errCode">系统错误码</param>
-        /// <returns></returns>
-        public static string GetLastErrorString(int errCode)
-        {
-            IntPtr tempptr = IntPtr.Zero;
-            string msg = null;
-            FormatMessage(0x1300, ref tempptr, errCode, 0, ref msg, 255, ref tempptr);
-            return msg;
-        }
-
-        [System.Runtime.InteropServices.DllImport("Kernel32.dll")]
-        public extern static int FormatMessage(int flag, ref IntPtr source, int msgid, int langid, ref string buf, int size, ref IntPtr args);
-
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr GetParent(IntPtr hwnd);
-        ///// <summary>
-        ///// ShellExecute(IntPtr.Zero, "Open", "C:/Program Files/TTPlayer/TTPlayer.exe", "", "", 1);
-        ///// </summary>
-        ///// <param name="hwnd"></param>
-        ///// <param name="lpOperation"></param>
-        ///// <param name="lpFile"></param>
-        ///// <param name="lpParameters"></param>
-        ///// <param name="lpDirectory"></param>
-        ///// <param name="nShowCmd"></param>
-        ///// <returns></returns>
-        //[DllImport("shell32.dll", EntryPoint = "ShellExecute")]
-        //public static extern int ShellExecute(
-        //IntPtr hwnd,
-        //string lpOperation,
-        //string lpFile,
-        //string lpParameters,
-        //string lpDirectory,
-        //int nShowCmd
-        //);
-        //[DllImport("kernel32.dll")]
-        //public static extern int OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId); 
-        [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-
-
-        private const int SWP_NOOWNERZORDER = 0x200;
-        private const int SWP_NOREDRAW = 0x8;
-        private const int SWP_NOZORDER = 0x4;
-        private const int SWP_SHOWWINDOW = 0x0040;
-        private const int WS_EX_MDICHILD = 0x40;
-        private const int SWP_FRAMECHANGED = 0x20;
-        private const int SWP_NOACTIVATE = 0x10;
-        private const int SWP_ASYNCWINDOWPOS = 0x4000;
-        private const int SWP_NOMOVE = 0x2;
-        private const int SWP_NOSIZE = 0x1;
-        private const int GWL_STYLE = (-16);
-        private const int WS_VISIBLE = 0x10000000;
-        private const int WM_CLOSE = 0x10;
-        private const int WS_CHILD = 0x40000000;
-
-        private const int  SW_HIDE            = 0; //{隐藏, 并且任务栏也没有最小化图标}
-        private const int  SW_SHOWNORMAL      = 1; //{用最近的大小和位置显示, 激活}
-        private const int  SW_NORMAL          = 1; //{同 SW_SHOWNORMAL}
-        private const int  SW_SHOWMINIMIZED   = 2; //{最小化, 激活}
-        private const int  SW_SHOWMAXIMIZED   = 3; //{最大化, 激活}
-        private const int  SW_MAXIMIZE        = 3; //{同 SW_SHOWMAXIMIZED}
-        private const int  SW_SHOWNOACTIVATE  = 4; //{用最近的大小和位置显示, 不激活}
-        private const int  SW_SHOW            = 5; //{同 SW_SHOWNORMAL}
-        private const int  SW_MINIMIZE        = 6; //{最小化, 不激活}
-        private const int  SW_SHOWMINNOACTIVE = 7; //{同 SW_MINIMIZE}
-        private const int  SW_SHOWNA          = 8; //{同 SW_SHOWNOACTIVATE}
-        private const int  SW_RESTORE         = 9; //{同 SW_SHOWNORMAL}
-        private const int  SW_SHOWDEFAULT     = 10; //{同 SW_SHOWNORMAL}
-        private const int  SW_MAX             = 10; //{同 SW_SHOWNORMAL}
-
-        //const int PROCESS_ALL_ACCESS = 0x1F0FFF;
-        //const int PROCESS_VM_READ = 0x0010;
-        //const int PROCESS_VM_WRITE = 0x0020;     
-        #endregion Win32 API
-
-
+      
         public void EmbedAgain()
         {
             EmbedProcess(m_AppProcess, this);
@@ -364,7 +234,7 @@ namespace SmileWei.EmbeddedApp
             try
             {
                 // Put it into this form
-                embedResult = SetParent(app.MainWindowHandle, control.Handle);
+                embedResult = Win32API.SetParent(app.MainWindowHandle, control.Handle);
                 //if (embedResult != 0)
                 //{ state = EmbedState.embeded; }
                 //else
@@ -375,21 +245,21 @@ namespace SmileWei.EmbeddedApp
             try
             {
                 // Remove border and whatnot               
-                SetWindowLong(new HandleRef(this,app.MainWindowHandle), GWL_STYLE, WS_VISIBLE);
+                Win32API.SetWindowLong(new HandleRef(this, app.MainWindowHandle), Win32API.GWL_STYLE, Win32API.WS_VISIBLE);
             }
             catch (Exception)
             { }
             try
             {
                 // Move the window to overlay it on this window
-                MoveWindow(app.MainWindowHandle, 0, 0, control.Width, control.Height, true);
+                Win32API.MoveWindow(app.MainWindowHandle, 0, 0, control.Width, control.Height, true);
             }
             catch (Exception)
             { }
 
             if (ShowEmbedResult)
             {
-                var errorString = AppContainer.GetLastError();
+                var errorString = Win32API.GetLastError();
                 MessageBox.Show(errorString);
             }
 
@@ -411,5 +281,10 @@ namespace SmileWei.EmbeddedApp
         /// Show a MessageBox to tell whether the embedding is successfully done.
         /// </summary>
         public bool ShowEmbedResult { get; set; }
+
+        public static void SetWindowLong(HandleRef handleRef, int GWL_STYLE, int WS_VISIBLE)
+        {
+            Win32API.SetWindowLong(handleRef, GWL_STYLE, WS_VISIBLE);
+        }
     }
 }
---
+++ SmileWei.EmbeddedApp/Win32API.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SmileWei.EmbeddedApp
+{
+    class Win32API
+    {
+        #region Win32 API
+        [DllImport("user32.dll", EntryPoint = "GetWindowThreadProcessId", SetLastError = true,
+             CharSet = CharSet.Unicode, ExactSpelling = true,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern long GetWindowThreadProcessId(long hWnd, long lpdwProcessId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongA", SetLastError = true)]
+        public static extern long GetWindowLong(IntPtr hwnd, int nIndex);
+
+        public static IntPtr SetWindowLong(HandleRef hWnd, int nIndex, int dwNewLong)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return SetWindowLongPtr32(hWnd, nIndex, dwNewLong);
+            }
+            return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+        }
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetWindowLongPtr32(HandleRef hWnd, int nIndex, int dwNewLong);
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern long SetWindowPos(IntPtr hwnd, long hWndInsertAfter, long x, long y, long cx, long cy, long wFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hwnd, int x, int y, int cx, int cy, bool repaint);
+
+        [DllImport("user32.dll", EntryPoint = "PostMessageA", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hwnd, uint Msg, uint wParam, uint lParam);
+
+        /// <summary>
+        /// 获取系统错误信息描述
+        /// </summary>
+        /// <param name="errCode">系统错误码</param>
+        /// <returns></returns>
+        public static string GetLastError()
+        {
+            var errCode = Marshal.GetLastWin32Error();
+            IntPtr tempptr = IntPtr.Zero;
+            string msg = null;
+            FormatMessage(0x1300, ref tempptr, errCode, 0, ref msg, 255, ref tempptr);
+            return msg;
+        }
+        /// <summary>
+        /// 获取系统错误信息描述
+        /// </summary>
+        /// <param name="errCode">系统错误码</param>
+        /// <returns></returns>
+        public static string GetLastErrorString(int errCode)
+        {
+            IntPtr tempptr = IntPtr.Zero;
+            string msg = null;
+            FormatMessage(0x1300, ref tempptr, errCode, 0, ref msg, 255, ref tempptr);
+            return msg;
+        }
+
+        [System.Runtime.InteropServices.DllImport("Kernel32.dll")]
+        public extern static int FormatMessage(int flag, ref IntPtr source, int msgid, int langid, ref string buf, int size, ref IntPtr args);
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetParent(IntPtr hwnd);
+        ///// <summary>
+        ///// ShellExecute(IntPtr.Zero, "Open", "C:/Program Files/TTPlayer/TTPlayer.exe", "", "", 1);
+        ///// </summary>
+        ///// <param name="hwnd"></param>
+        ///// <param name="lpOperation"></param>
+        ///// <param name="lpFile"></param>
+        ///// <param name="lpParameters"></param>
+        ///// <param name="lpDirectory"></param>
+        ///// <param name="nShowCmd"></param>
+        ///// <returns></returns>
+        //[DllImport("shell32.dll", EntryPoint = "ShellExecute")]
+        //public static extern int ShellExecute(
+        //IntPtr hwnd,
+        //string lpOperation,
+        //string lpFile,
+        //string lpParameters,
+        //string lpDirectory,
+        //int nShowCmd
+        //);
+        //[DllImport("kernel32.dll")]
+        //public static extern int OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId); 
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+
+        public const int SWP_NOOWNERZORDER = 0x200;
+        public const int SWP_NOREDRAW = 0x8;
+        public const int SWP_NOZORDER = 0x4;
+        public const int SWP_SHOWWINDOW = 0x0040;
+        public const int WS_EX_MDICHILD = 0x40;
+        public const int SWP_FRAMECHANGED = 0x20;
+        public const int SWP_NOACTIVATE = 0x10;
+        public const int SWP_ASYNCWINDOWPOS = 0x4000;
+        public const int SWP_NOMOVE = 0x2;
+        public const int SWP_NOSIZE = 0x1;
+        public const int GWL_STYLE = (-16);
+        public const int WS_VISIBLE = 0x10000000;
+        public const int WM_CLOSE = 0x10;
+        public const int WS_CHILD = 0x40000000;
+
+        public const int SW_HIDE = 0; //{隐藏, 并且任务栏也没有最小化图标}
+        public const int SW_SHOWNORMAL = 1; //{用最近的大小和位置显示, 激活}
+        public const int SW_NORMAL = 1; //{同 SW_SHOWNORMAL}
+        public const int SW_SHOWMINIMIZED = 2; //{最小化, 激活}
+        public const int SW_SHOWMAXIMIZED = 3; //{最大化, 激活}
+        public const int SW_MAXIMIZE = 3; //{同 SW_SHOWMAXIMIZED}
+        public const int SW_SHOWNOACTIVATE = 4; //{用最近的大小和位置显示, 不激活}
+        public const int SW_SHOW = 5; //{同 SW_SHOWNORMAL}
+        public const int SW_MINIMIZE = 6; //{最小化, 不激活}
+        public const int SW_SHOWMINNOACTIVE = 7; //{同 SW_MINIMIZE}
+        public const int SW_SHOWNA = 8; //{同 SW_SHOWNOACTIVATE}
+        public const int SW_RESTORE = 9; //{同 SW_SHOWNORMAL}
+        public const int SW_SHOWDEFAULT = 10; //{同 SW_SHOWNORMAL}
+        public const int SW_MAX = 10; //{同 SW_SHOWNORMAL}
+
+        //const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+        //const int PROCESS_VM_READ = 0x0010;
+        //const int PROCESS_VM_WRITE = 0x0020;     
+        #endregion Win32 API
+
+
+    }
+}
